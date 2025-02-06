7894251b34762d5ad3bf262d059a147cf9af9bb1
Baldan661 <telmnsmurf@gmail.com>
2025-02-06 12:47:21 +0800

    BaldanDev.py

--- BaldanDev.py
+++ BaldanDev.py
@@ -10,7 +10,7 @@ import requests
 
 __ENDPOINT_URL__: str = "https://telmunn.squareweb.app/api"
 
-class BaldanDev:
+class Telmunnshop:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
